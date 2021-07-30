47760d8c0639fe7e030c6be7456d0eeffd807490
Imho <88206520+imh0s@users.noreply.github.com>
2021-07-30 20:34:58 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+function insert(num) {
+  document.form.textview.value = document.form.textview.value + num;
+}
+
+function clean() {
+  document.form.textview.value = "";
+}
+
+function back() {
+  var exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0, exp.length - 1);
+}
+
+function equal() {
+  var exp = document.form.textview.value;
+  if (exp) {
+    document.form.textview.value = eval(exp);
+  }
+}
+
+window.insert = insert;
+window.clean = clean;
+window.back = back;
+window.equal = equal;
